d718e01afac4558e47fb15a8cffa2b743c6d48df
Tiago da Cruz Santos <37669281+TiagoCruzSantos@users.noreply.github.com>
2022-12-07 03:28:06 +0000

    prod, migrations

--- bees-backend/ormconfig.ts
+++ bees-backend/ormconfig.ts
@@ -1,15 +1,16 @@
 import { DataSource } from "typeorm"
 import * as dotenv from 'dotenv'
+import * as path from 'path'
 
 dotenv.config()
 export default new DataSource({
     type: "postgres",
-    host: process.env["HOST"],
+    host: process.env["NODE_ENV"] === "prod" ? process.env["POSTGRES_HOST"] : "localhost",
     port: 5432,
     username: process.env["POSTGRES_USER"],
     password: process.env["POSTGRES_PASSWORD"],
     database: process.env["POSTGRES_USER"],
-    entities: ["./src/presistence/models/*{.ts, .js}"],
-    migrations: ["./migrations/*{.ts, .js}"],
+    entities: [path.join(__dirname, "/src", "/presistence", "/models", "/*{.ts, .js}")],
+    migrations: [path.join(__dirname, "/migrations", "/*{.ts, .js}")],
     migrationsTableName: "migration_table"
 })
